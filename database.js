f15ef1137adb8d0db13d85be8fa0729523ee4222
quachvankhanh <khanhqvj@gmail.com>
2025-02-17 14:34:44 +0700

    change folder path database

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
 const bcrypt = require("bcrypt");
 const path = require("path");
-const dbPath = path.join(__dirname, "../../data/database.db");
+const dbPath = path.join(__dirname, "../data/database.db");
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Error connecting to database:", err.message);
